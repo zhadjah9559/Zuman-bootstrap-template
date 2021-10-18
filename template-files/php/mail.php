0fcd872f7e6662f9248bc98b1375f7e16a1b4607
Zachary_Hadjah <Zachary_Hadjah@Dellteam.com>
2021-10-18 18:40:29 -0500

    changed email

--- template-files/php/mail.php
+++ template-files/php/mail.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@sojonever.com";
+        $recipient = "Nicholas1122yahoo.com";
 
         // Set the email subject.
         $subject = "New Message from niz by - $name";
